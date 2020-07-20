4db59627a854b8ac17c7aa0cb1d2b07071ec752a
Tom Brouwer <git@tombrouwer.dev>
2020-07-20 19:45:56 +0200

    Build

--- src/Commit.js
+++ src/Commit.js
@@ -5,9 +5,7 @@ import * as Notifier from "./Definitions/Core";
  *
  * @constructor
  * @param {string} path The path to the config file.
- * @param {function} info Function to give process info.
- * @param {function} output Function to output data.
- * @param {function} debug Function to debug.
+ * @param {Object} notifier
  */
 export default class Commit {
   constructor(commit, notifier = Notifier) {
@@ -16,71 +14,5 @@ export default class Commit {
     this.output = notifier.output;
     this.debug = notifier.debug;
     this.debug(`CONFIG PATH`, commit);
-  }
-
-  /**
-   * Static function to construct, load, parse, validate and merge.
-   *
-   * @param {string} path The path to the config file.
-   * @param {Object} notifier Notifier object with log and output functions.
-   *
-   * @return {Config}
-   */
-  // static construct(path, notifier = Notifier) {
-  //   return new Config(path, notifier)
-  //     .load()
-  //     .parse()
-  //     .validate()
-  //     .merge();
-  // }
-  static fetchCommitData(commit, args, octokit) {
-    args.ref = commit.id || commit.sha;
-
-    return octokit.repos.getCommit(args);
-  }
-
-  static async get(context, args, octokit, notifier = Notifier) {
-    let commits;
-
-    notifier.debug(`Getting commits...`);
-
-    switch (context.eventName) {
-      case `push`:
-        commits = context.payload.commits;
-        break;
-
-      case `pull_request`:
-        var url = context.payload.pull_request.commits_url;
-
-        commits = await octokit.paginate(`GET ${url}`, args);
-        break;
-
-      default:
-        notifier.info(
-          `You are using this action on an event for which it has not been tested. Only the "push" and "pull_request" events are officially supported.`,
-        );
-
-        commits = [];
-        break;
-    }
-
-    commits = commits.filter(
-      c => (!c.parents || 1 === c.parents.length) && c.distinct,
-    );
-
-    const commitsDataFetchers = commits.map(commit =>
-      Commit.fetchCommitData(commit, args, octokit),
-    );
-
-    let commitsData;
-    Promise.all(commitsDataFetchers)
-      .then(data => {
-        commitsData = data;
-      })
-      .catch(error => {
-        notifier.info(`ERROR GET COMMITS`, error);
-      });
-
-    return commitsData.map(commitData => new Commit(commitData, notifier));
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,43 +4,47 @@ import { context } from "@actions/github";
 import { Octokit } from "@octokit/rest";
 import Config from "./Config";
 import Package from "./Package";
-import Commit from "./Commit";
+import CommitCollection from "./CommitCollection";
 import * as Notifier from "./Definitions/Core";
 
 const CONFIG_PATH = input(`path_to_config`);
 const GITHUB_TOKEN = input(`github_token`);
 info(`Test line 0`);
 
-try {
-  info(`Test line 1`);
-  debug(`Test line 1`);
-  const repository = context.payload.repository;
-  info(`Test line 2`);
-  const owner = context.payload.repository.owner;
-  info(`Test line 3`);
-  const args = { owner: owner.name || owner.login, repo: repository.name };
-  info(`Test line 4`);
-  const octokit = new Octokit(GITHUB_TOKEN);
-  info(`Test line 5`);
-  const config = Config.construct(CONFIG_PATH);
-  info(`Test line 6`);
-  const packages = Package.getWithPaths(config.packagePaths);
-  Notifier.info(Notifier.toJSON(packages));
-  info(`Test line 7`);
-  Commit.get(context, args, octokit)
-    .then(commits => {
-      info(Notifier.toJSON(commits));
-    })
-    .catch(error => {
-      showError(error);
-      throw error;
-    });
+const start = async () => {
+  try {
+    info(`Test line 1`);
+    debug(`Test line 1`);
+    const repository = context.payload.repository;
+    info(`Test line 2`);
+    const owner = context.payload.repository.owner;
+    info(`Test line 3`);
+    const args = { owner: owner.name || owner.login, repo: repository.name };
+    info(`Test line 4`);
+    const octokit = new Octokit(GITHUB_TOKEN);
+    info(`Test line 5`);
+    const config = Config.construct(CONFIG_PATH);
+    info(`Test line 6`);
+    const packages = Package.getWithPaths(config.packagePaths);
+    Notifier.info(Notifier.toJSON(packages));
+    info(`Test line 7`);
 
-  // Get and analize packages.
-  // Get file changes
-  // Filter packages where files are changed
-  // Run commands on packages
-} catch (error) {
-  showError(error);
-  fail(error.message);
-}
+    const commitCollection = await new CommitCollection(
+      context.payload.commits,
+      args,
+      octokit,
+    ).load();
+
+    Notifier.info(Notifier.toJSON(commitCollection));
+
+    // Get and analize packages.
+    // Get file changes
+    // Filter packages where files are changed
+    // Run commands on packages
+  } catch (error) {
+    showError(error);
+    fail(error.message);
+  }
+};
+
+start();
