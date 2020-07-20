ff51f0a56f40dfc30a6d3d0fc206dce0545502a6
Tom Brouwer <git@tombrouwer.dev>
2020-07-20 17:34:34 +0200

    Update logging to debug

--- src/index.js
+++ src/index.js
@@ -14,17 +14,23 @@ try {
   info(`Test line 1`);
   debug(`Test line 1`);
   const repository = context.payload.repository;
-  const owner = context.payload.repository.owner;
-  const args = { owner: owner.name || owner.login, repo: repository.name };
-  const octokit = getOctokit(GITHUB_TOKEN);
-  const config = Config.construct(CONFIG_PATH);
-  const packages = Package.getWithPaths(config.packagePaths);
   info(`Test line 2`);
+  const owner = context.payload.repository.owner;
+  info(`Test line 3`);
+  const args = { owner: owner.name || owner.login, repo: repository.name };
+  info(`Test line 4`);
+  const octokit = getOctokit(GITHUB_TOKEN);
+  info(`Test line 5`);
+  const config = Config.construct(CONFIG_PATH);
+  info(`Test line 6`);
+  const packages = Package.getWithPaths(config.packagePaths);
+  info(`Test line 7`);
   Commit.getCommits(context, args, octokit)
     .then(commits => {
       Notifier.info(Notifier.toJSON(commits));
     })
     .catch(error => {
+      showError(error);
       throw error;
     });
 
@@ -32,7 +38,6 @@ try {
   // Get file changes
   // Filter packages where files are changed
   // Run commands on packages
-  info(`Test line 3`);
   info(packages);
 } catch (error) {
   showError(error);
