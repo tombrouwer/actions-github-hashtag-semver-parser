6bb5b68c54bb135a210348c4c2cbf27be15fcb17
Tom Brouwer <git@tombrouwer.dev>
2020-07-20 22:26:55 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "regenerator-runtime/runtime";
-import { info, fail, input, toJSON } from "./Core";
+import { info, fail, input, toJSON, output } from "./Core";
 import { context } from "@actions/github";
+import map from "lodash/map";
 
 const DEFAULT_SEMVER = input(`default_semver`);
 info(`Test line 0`);
@@ -8,7 +9,24 @@ info(`Test line 0`);
 const start = async () => {
   try {
     info(toJSON(context.payload.commits));
-    info(DEFAULT_SEMVER);
+    const messages = map(context.payload.commits, `message`);
+    const messagesStr = messages.implode(` `);
+    const semver =
+      messagesStr.search(`#minor`) > -1
+        ? `minor`
+        : messagesStr.search(`#major`) > -1
+        ? `major`
+        : messagesStr.search(`#patch`) > -1
+        ? `patch`
+        : DEFAULT_SEMVER;
+
+    if (!semver) {
+      throw new Error(`No semver found!`);
+    }
+
+    output(`semver`, semver);
+
+    info(`semver`, semver);
   } catch (error) {
     fail(error.message);
   }
