d9997866945cffc3aeea9f8b6edbe90549e463c0
Tom Brouwer <git@tombrouwer.dev>
2020-07-20 22:35:26 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 import "regenerator-runtime/runtime";
-import { info, fail, input, toJSON, output, showError } from "./Core";
+import { info, fail, input, output, showError } from "./Core";
 import { context } from "@actions/github";
 import map from "lodash/map";
 
 const DEFAULT_SEMVER = input(`default_semver`);
 
-const start = async () => {
-  try {
-    const messages = map(context.payload.commits, `message`);
-    const messagesStr = messages.implode(` `);
-    const semver =
-      messagesStr.search(`#minor`) > -1
-        ? `minor`
-        : messagesStr.search(`#major`) > -1
-        ? `major`
-        : messagesStr.search(`#patch`) > -1
-        ? `patch`
-        : DEFAULT_SEMVER;
+try {
+  const messages = map(context.payload.commits, `message`);
+  const messagesStr = messages.implode(` `);
+  const semver =
+    messagesStr.search(`#minor`) > -1
+      ? `minor`
+      : messagesStr.search(`#major`) > -1
+      ? `major`
+      : messagesStr.search(`#patch`) > -1
+      ? `patch`
+      : DEFAULT_SEMVER;
 
-    if (!semver) {
-      showError(`No semver found!`);
-      throw new Error(`No semver found!`);
-    }
-
-    output(`semver`, semver);
-
-    info(`semver`, semver);
-  } catch (error) {
-    fail(error.message);
+  if (!semver) {
+    showError(`No semver found!`);
+    throw new Error(`No semver found!`);
   }
-};
 
-start();
+  output(`semver`, semver);
+
+  info(`semver`, semver);
+} catch (error) {
+  fail(error.message);
+}
